1188b83d9f56ab097e385e5a007041403140fcae
sabaly_mac <tedoldhead@gmail.com>
2023-03-25 22:19:29 +0800

    v2 add xy and title label

--- matplot.py
+++ matplot.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares)
+#設定線條粗細（linewidth）
+plt.plot(squares, linewidth=5)
+#設置圖表標題，x,y軸的的標籤
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Squares of Value", fontsize=14)
+
+#設置刻度標記的大小
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
