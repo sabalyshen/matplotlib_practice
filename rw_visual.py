69adc41df10540ec6fa9698b9d7bb2625e4b80d9
sabaly_mac <tedoldhead@gmail.com>
2023-03-26 22:34:08 +0800

    add hide x,y axis

--- rw_visual.py
+++ rw_visual.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, c=rw.y_values, cmap=plt.cm.Purples, s=15)
-plt.show()
+
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Purples, s=15)
+    #突出起點跟終點
+    plt.scatter(rw.x_values[0], rw.y_values[0], c='b', s=100)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='r', s=100)
+    
+    #隱藏座標軸
+    plt.axis('off')
+    
+    plt.show()
+    
+    keep_running = input("Make another walk?(y/n) ")
+    if keep_running =='n':
+        break
