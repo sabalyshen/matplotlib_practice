dc9455215ed664a4ae2c7caf46d81136baa62f51
sabaly_mac <tedoldhead@gmail.com>
2023-03-26 19:18:48 +0800

    v4 add multi point

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,4 +1,15 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s=100)
+#s=100調整點的大小
+#設置圖表標題並給座標軸加上標籤
+plt.title("Square Number", fontsize=24)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Squares of Value", fontsize=14)
+
+#設置刻度標記的大小
+plt.tick_params(axis='both', which='major', labelsize=14)
 plt.show()
