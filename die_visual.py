338a7415b0d069f68eb87715e12003255158929a
sabalyshen <tedoldhead@gmail.com>
2023-03-29 22:23:07 +0800

    added two D6

--- die_visual.py
+++ die_visual.py
@@ -1,18 +1,20 @@
 import pygal
 from die import Die
 
-#創建一個D6
-die = Die()
+#創建2個D6
+die_1 = Die()
+die_2 = Die()
 
 #將骰子值出的數字存在list裡面
 results = []
 
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 frequencies = []
-for value in range(1, die.num_sides+1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -20,10 +22,10 @@ print(frequencies)
 
 #對結果可視化
 hist = pygal.Bar()
-hist.title = "Results of rolling one D6 1000 times. "
-hist.x_labels = [1, 2, 3, 4, 5, 6]
+hist.title = "Results of rolling two D6 1000 times. "
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6', frequencies)
+hist.add('D6 + D6', frequencies)
 hist.render_to_file('die_visual.svg')
